e64af065282720bf733a6b4f089d3493e831fc6c
chanthida <chanthida.adulwittayakorn@gmail.com>
2017-04-20 16:38:33 +0700

    add

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -36,7 +36,7 @@
 
                     <!-- Branding Image -->
                     <a class="navbar-brand" href="{{ url('/') }}">
-                        {{ config('app.name', 'Laravel') }} 5.4
+                        {{ config('app.name', 'Laravel') }}
                     </a>
                 </div>
 
